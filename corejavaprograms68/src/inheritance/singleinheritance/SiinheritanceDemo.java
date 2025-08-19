4222561d12395ac885041c8ddf0971551699a92d
NEW <NEW@DESKTOP-VJVAK9A>
2025-08-19 15:45:12 +0530

    corejavaprograms - Single  inheritance

---
+++ corejavaprograms68/src/inheritance/singleinheritance/SiinheritanceDemo.java
@@ -0,0 +1,17 @@
+package inheritance.singleinheritance;
+
+
+
+	public class SiinheritanceDemo {
+
+		public static void main(String[] args) {
+			
+			Trainer t = new Trainer(001,"kesava","Puducherry",01,"SOFTWARE DEVELOPER","Chennai");
+			System.out.println(t);
+			
+			Person a = new Person(113,"kesava","Puducherry");
+			System.out.println(a);
+			
+		}
+
+}
